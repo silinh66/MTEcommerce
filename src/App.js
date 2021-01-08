52cac55e9444a90ec89a158fb66a20cbad843bc1
linhns <silinh66@gmail.com>
2021-01-09 00:10:13 +0700

    Review

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { commerce } from "./lib/commerce";
-import { Products, Navbar, Cart } from "./components";
+import { Products, Navbar, Cart, Checkout } from "./components";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
@@ -59,6 +59,9 @@ const App = () => {
               handleRemoveFromCart={handleRemoveFromCart}
               handleEmptyCart={handleEmptyCart}
             />
+          </Route>
+          <Route exact path="/checkout">
+            <Checkout cart={cart} />
           </Route>
         </Switch>
       </div>
